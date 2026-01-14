4e19eccba393c699d1d72004067907e337cb8848
Your Name <you@example.com>
2026-01-14 15:46:00 +0500

    changed user model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,41 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email обязателен")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Superuser должен иметь is_staff=True")
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Superuser должен иметь is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
